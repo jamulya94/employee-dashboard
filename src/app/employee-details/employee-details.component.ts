a3e793e5a3fe5743e49b0f44fd9bfca56ad68de8
jamulya94 <j.amulya94@gmail.com>
2022-04-19 22:24:21 +0530

    2 files modified

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-details.component.css']
 })
 export class EmployeeDetailsComponent implements OnInit {
-
+data = [{
+  time1:'11:00PM',
+  time2:'7:00AM',
+  position:'BM'
+},
+{
+  time1:'11:00PM',
+  time2:'7:00AM',
+  position:'ZT'
+}
+]
   constructor() { }
 
   ngOnInit() {
